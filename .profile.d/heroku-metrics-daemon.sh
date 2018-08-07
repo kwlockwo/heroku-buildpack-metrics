1dd5e445a1cb192ea24dc654b5cd37ab5255b3d2
Keith Lockwood <kwlockwo@gmail.com>
2018-08-07 15:30:04 +1000

    Update heroku-metrics-daemon.sh

--- .profile.d/heroku-metrics-daemon.sh
+++ .profile.d/heroku-metrics-daemon.sh
@@ -5,6 +5,8 @@ setup_metrics() {
     if [[ -z "$HEROKU_METRICS_URL" ]] || [[ "${DYNO}" = run\.* ]]; then
         return 0
     fi
+    
+    echo "! Debug-1 !"
 
     STARTTIME=$(date +%s)
     BUILD_DIR=/tmp
@@ -14,10 +16,14 @@ setup_metrics() {
         echo "!!!!! Failed to find latest agentmon. Please report this as a bug. Metrics collection will be disabled this run."
         return 1
     fi
+    
+    echo "! Debug-2 !"
 
     BASENAME=$(basename "${DOWNLOAD_URL}")
 
     curl -L --retry 3 -s -o "${BUILD_DIR}/${BASENAME}" "${DOWNLOAD_URL}"
+
+    echo "! Debug-3 !"
 
     # Ensure the bin folder exists, if not already.
     mkdir -p "${BUILD_DIR}/bin"
@@ -25,6 +31,8 @@ setup_metrics() {
     # Extract agentmon release
     tar --warning=no-unknown-keyword -C "${BUILD_DIR}/bin" -zxf "${BUILD_DIR}/${BASENAME}"
     chmod +x "${BUILD_DIR}/bin/agentmon"
+    
+    echo "! Debug-4 !"
 
     ELAPSEDTIME=$(($(date +%s) - STARTTIME))
     echo "agentmon setup took ${ELAPSEDTIME} seconds"
@@ -35,6 +43,8 @@ setup_metrics() {
         AGENTMON_FLAGS+=("-debug")
     fi
 
+    echo "! Debug-5 !"
+
     if [[ -x "${BUILD_DIR}/bin/agentmon" ]]; then
         (while true; do
             ${BUILD_DIR}/bin/agentmon "${AGENTMON_FLAGS[@]}" "${HEROKU_METRICS_URL}"
@@ -44,6 +54,8 @@ setup_metrics() {
     else
         echo "No agentmon executable found. Not starting."
     fi
+    
+    echo "! Debug-6 !"
 }
 
 setup_metrics
